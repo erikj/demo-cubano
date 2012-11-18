45ecf69e893126384691d744e73067b6c279ae23
erikj <erik@erikj.me>
2012-11-18 16:43:34 -0700

    adding 'readme' action that renders README.md

--- hello-world.rb
+++ hello-world.rb
@@ -1,6 +1,8 @@
 require "cuba"
+require "cuba/render"
 
-Cuba.use Rack::Session::Cookie
+Cuba.plugin Cuba::Render
+# Cuba.use Rack::Session::Cookie
 
 Cuba.define do
   on get do
@@ -11,18 +13,9 @@ Cuba.define do
     on root do
       res.redirect "/hello"
     end
+
+    on "readme" do
+      res.write render 'README.md'
+    end
   end
 end
-
-# cat hello_world_test.rb
-# require "cuba/test"
-
-# scope do
-#   test "Homepage" do
-#     get "/"
-
-#     follow_redirect!
-
-#     assert_equal "Hello world!", last_response.body
-#   end
-# end
